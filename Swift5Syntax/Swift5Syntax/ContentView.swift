474f1b4c758d23f40a04d1a369d59105004f409b
Aki Xie <aki.c.xie@gmail.com>
2020-04-07 11:24:04 +0800

    xcode new project

---
+++ Swift5Syntax/Swift5Syntax/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift5Syntax
+//
+//  Created by akixie on 2020/4/7.
+//  Copyright © 2020 Aki.Xie. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
